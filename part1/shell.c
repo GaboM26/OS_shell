e0f9f1464a817a4b2a7f930b0c6553fde016a399
David Gabriel Millares Bellido <dgm2148@columbia.edu>
2022-09-12 21:51:32 -0400

    Shell can now open program with no args + extra makefile parts

--- part1/shell.c
+++ part1/shell.c
@@ -1,11 +1,51 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/types.h>
 
+static void die(const char *err){
+    perror(err);
+    exit(-1);
+}
+
+void processInput(char *);
+void removeNL(char *);
 int main(){
-    while(1){
-        char inpt[1000];
+    char inpt[1000];
+    printf("$");
+    while(fgets(inpt, sizeof(inpt), stdin) != NULL){
+        processInput(inpt);
         printf("$");
-        fgets(inpt, sizeof(inpt), stdin);
-        
     }
+}
+
+void processInput(char *inpt){
+    int st; 
+    char *tok = " ";
+    char *cmd = strtok(inpt, tok);    
+
+    if(strchr(cmd, '\n') != NULL)
+        removeNL(cmd);
+
+    pid_t pid = fork();
+
+    if(pid < 0){
+        die("ERROR fork");
+    }
+    else if(pid == 0){ //child process
+        execl(cmd, cmd, (char *) 0);
+        die("ERROR");
+    }
+    else{ //parent process
+        if(waitpid(pid, &st, 0) != pid){
+            perror("ERROR: waitpid");
+        }
+    }
+
+}
+
+void removeNL(char *str){
+    str[strlen(str)-1] = 0;
 }
