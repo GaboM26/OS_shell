b596a493fb956d2a1ab87ae067dc39b4fcef19b0
David Gabriel Millares Bellido <dgm2148@columbia.edu>
2022-09-19 18:52:11 -0400

    slightly reformatted part1 code

--- part1/shell.c
+++ part1/shell.c
@@ -28,7 +28,6 @@ int main(){
     int k;
     char *inpt = malloc(size);
 
-    //In case malloc failed
     if(inpt == NULL){
         perror("malloc failed\n");
         exit(-1);
@@ -36,7 +35,9 @@ int main(){
 
     printf("$");
     while((k = getline(&inpt, &size, stdin)) > 0){
-        //in case getline used realloc
+
+        /*in case getline used realloc*/
+        
         if(inpt == NULL){
             perror("malloc failed\n");
             exit(-1);
@@ -50,31 +51,29 @@ int main(){
     if(k == -1 && errno != 0){
         errPrint(1);
     }
-    //if errno is 0 then no error occured
+    /*if errno is 0 then no error occured*/
     myExit(inpt);
-    
+
 }
 
-//something was entered as input (no blank line)
-//checks if it is valid and sends it to appropriate function
+/*something was entered as input (no blank line)
+checks if it is valid and sends it to appropriate function */
 void validInput(char *inpt){
 
-    //means it is trying to go to a folder and run executable
     if(inpt[0]=='/'){
         runProg(inpt);
         return ;
     }
 
-    //Manipulate input now that we know its not trying to go to folder
+    /*Manipulate input now that we know its not trying to go to folder*/
     char myInpt[strlen(inpt) + 1];
     strcpy(myInpt, inpt);
 
-    //divide into tokens
     char *spcmd = strtok(myInpt, " ");
 
     if(strchr(spcmd, '\n') != NULL)
         removeNL(spcmd);
-    //If you are exiting, you pass original pointer
+
     if(!strcmp(spcmd, "exit"))
         myExit(inpt);
     else if(!strcmp(spcmd, "cd"))
@@ -83,7 +82,7 @@ void validInput(char *inpt){
         errPrint(0);
 }
 void runProg(char *inpt){
-    //get command and save a copy to execute (manipulated pointers)
+    /*get command and save a copy to execute (manipulated pointers)*/
     int st; 
     char *tok = " ";
     char *args[5000];
@@ -91,34 +90,31 @@ void runProg(char *inpt){
     char prgrm[strlen(cmd) + 1]; 
     strcpy(prgrm, cmd);
 
-    //parse and get all command line args
+    /*parse and get all command line args*/
     int i = 1;
     while((args[i] = strtok(NULL, tok)) != NULL)
         i++;
-    args[0] = progName(cmd); //make first arg program name
-    
-    // if input is one command, take out the \n from it
+    args[0] = progName(cmd); 
+
     if(strchr(prgrm, '\n') != NULL)
         removeNL(prgrm);
 
-    //Do the same for final input
     if(strchr(args[i-1], '\n') != NULL)
         removeNL(args[i-1]);
 
     pid_t pid = fork();
 
-    //checks if fork failed
+    /*checks if fork failed*/
     if(pid < 0){
         perror("fork failed");
         myExit(inpt);
     }
-    else if(pid == 0){ //child process
+    else if(pid == 0){ /*child process*/
         execv(prgrm, args);
         die(inpt);
     }
-    else{ //parent process
+    else{ /*parent process*/
         if(waitpid(pid, &st, 0) != pid){
-            //checks if waitpid failed
             perror("waitpid failed");
             myExit(inpt);
         }
@@ -126,14 +122,13 @@ void runProg(char *inpt){
 
 }
 
-//removes next line from a string
-//Note that the only case this will happen is if one presses
-//enter at the very end due to how fgets works
+/*removes next line from a string
+ * Note that the only case this will happen is if one presses
+ * enter at the very end due to how fgets works*/
 void removeNL(char *str){
     str[strlen(str)-1] = 0;
 }
 
-//useful function in this case to print errors as required
 void errPrint(int realErr){
     if(realErr)
         fprintf(stderr, "error: %s\n", strerror(errno));
@@ -141,7 +136,7 @@ void errPrint(int realErr){
         fprintf(stderr, "error: unknown command\n");
 }
 
-//Returns pointer to only the program name given a path
+/*Returns pointer to only the program name given a path*/
 char *progName(char *path){
     char *delim = "/";
     char *name= strtok(path, delim);
@@ -149,7 +144,7 @@ char *progName(char *path){
     while((tmp=strtok(NULL, delim)) != NULL){
         name = tmp;
     }
-    //removes next line if necessary
+
     if(strchr(name, '\n') != NULL)
         removeNL(name);
     return name;
@@ -169,7 +164,7 @@ void myExit(char *inpt){
 }
 
 void cd(char *inpt){
-    
+
     if(inpt == NULL){
         errPrint(0);
         return ;
