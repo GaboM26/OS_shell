8f4240c3822321540d8f3b17bf3fa033813387f6
David Gabriel Millares Bellido <dgm2148@columbia.edu>
2022-09-21 16:53:11 -0400

    formatted shell.c

--- part1/shell.c
+++ part1/shell.c
@@ -1,191 +1,194 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<sys/types.h>
-#include<errno.h>
-#include<unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-void runProg(char *);
-void removeNL(char *);
-void errPrint(int );
-char *progName(char *);
-void validInput(char *);
-int min(int, int);
-void myExit(char *);
-void cd(char *);
+void runProg(char *inpt);
+void removeNL(char *str);
+void errPrint(int realErr);
+char *progName(char *path);
+void validInput(char *inpt);
+int min(int a, int b);
+void myExit(char *inpt);
+void cd(char *inpt);
 
-extern int errno;
 
-static void die(char *inpt){
-    errPrint(1);
-    myExit(inpt);
+static void die(char *inpt)
+{
+	errPrint(1);
+	myExit(inpt);
 }
 
-int main(){
-    size_t size = 100;
-    int k;
-    char *inpt = malloc(size);
+int main(void)
+{
+	size_t size = 100;
+	int k;
+	char *inpt = malloc(size);
 
-    if(inpt == NULL){
-        errPrint(1);
-        exit(EXIT_FAILURE);
-    }
+	if (inpt == NULL) {
+		errPrint(1);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("$");
-    if(ferror(stdout))
-        errPrint(1);
-    while((k = getline(&inpt, &size, stdin)) > 0){
+	printf("$");
+	if (ferror(stdout))
+		errPrint(1);
+	while ((k = getline(&inpt, &size, stdin)) > 0) {
 
-        /*in case getline used realloc*/
+		/*in case getline used realloc*/
 
-        if(inpt == NULL){
-            errPrint(1);
-            exit(EXIT_FAILURE);
-        }
+		if (inpt == NULL) {
+			errPrint(1);
+			exit(EXIT_FAILURE);
+		}
 
-        if(strcmp(inpt, "\n"))
-            validInput(inpt);
-        printf("$");
-        if(ferror(stdout))
-            errPrint(1);
-    }
+		if (strcmp(inpt, "\n"))
+			validInput(inpt);
+		printf("$");
+		if (ferror(stdout))
+			errPrint(1);
+	}
 
-    if(k == -1 && errno != 0){
-        errPrint(1);
-        exit(EXIT_FAILURE);
-    }
-    /*if errno is 0 then no error occured*/
-    myExit(inpt);
-
+	if (k == -1 && errno != 0) {
+		errPrint(1);
+		exit(EXIT_FAILURE);
+	}
+	/*if errno is 0 then no error occured*/
+	myExit(inpt);
 }
 
-/*something was entered as input (no blank line)
-  checks if it is valid and sends it to appropriate function */
-void validInput(char *inpt){
+/*
+ * something was entered as input (no blank line)
+ * checks if it is valid and sends it to appropriate function
+ */
+void validInput(char *inpt)
+{
 
-    if(inpt[0]=='/'){
-        runProg(inpt);
-        return ;
-    }
-    else if(inpt[0] == '.' && inpt[1] == '/'){
-        runProg(inpt);
-        return ;
-    }
+	if (inpt[0] == '/') {
+		runProg(inpt);
+		return;
+	} else if (inpt[0] == '.' && inpt[1] == '/') {
+		runProg(inpt);
+		return;
+	}
 
-    /*Manipulate input now that we know its not trying to go to folder*/
-    char myInpt[strlen(inpt) + 1];
-    strcpy(myInpt, inpt);
+	/*Manipulate input now that we know its not trying to go to folder*/
+	char myInpt[strlen(inpt) + 1];
 
-    char *spcmd = strtok(myInpt, " ");
+	strcpy(myInpt, inpt);
 
-    if(strchr(spcmd, '\n') != NULL)
-        removeNL(spcmd);
+	char *spcmd = strtok(myInpt, " ");
 
-    if(!strcmp(spcmd, "exit"))
-        myExit(inpt);
-    else if(!strcmp(spcmd, "cd"))
-        cd(strtok(NULL, " "));
-    else
-        errPrint(0);
-}
-void runProg(char *inpt){
-    /*get command and save a copy to execute (manipulated pointers)*/
-    int st; 
-    char *tok = " ";
-    char *args[5000];
-    char *cmd = strtok(inpt, tok);    
-    char prgrm[strlen(cmd) + 1]; 
-    strcpy(prgrm, cmd);
+	if (strchr(spcmd, '\n') != NULL)
+		removeNL(spcmd);
 
-    /*parse and get all command line args*/
-    int i = 1;
-    while((args[i] = strtok(NULL, tok)) != NULL)
-        i++;
-    args[0] = progName(cmd); 
-
-    if(strchr(prgrm, '\n') != NULL)
-        removeNL(prgrm);
-
-    if(strchr(args[i-1], '\n') != NULL)
-        removeNL(args[i-1]);
-
-    pid_t pid = fork();
-
-    /*checks if fork failed*/
-    if(pid < 0){
-        errPrint(1);
-        myExit(inpt);
-    }
-    else if(pid == 0){ /*child process*/
-        execv(prgrm, args);
-        die(inpt);
-    }
-    else{ /*parent process*/
-        if(waitpid(pid, &st, 0) != pid){
-            errPrint(1);
-            myExit(inpt);
-        }
-    }
-
+	if (!strcmp(spcmd, "exit"))
+		myExit(inpt);
+	else if (!strcmp(spcmd, "cd"))
+		cd(strtok(NULL, " "));
+	else
+		errPrint(0);
 }
 
-/*removes next line from a string
+void runProg(char *inpt)
+{
+	/*get command and save a copy to execute (manipulated pointers)*/
+	int st;
+	char *tok = " ";
+	char *args[5000];
+	char *cmd = strtok(inpt, tok);
+	char prgrm[strlen(cmd) + 1];
+
+	strcpy(prgrm, cmd);
+
+	/*parse and get all command line args*/
+	int i = 1;
+
+	while ((args[i] = strtok(NULL, tok)) != NULL)
+		i++;
+	args[0] = progName(cmd);
+
+	if (strchr(prgrm, '\n') != NULL)
+		removeNL(prgrm);
+
+	if (strchr(args[i - 1], '\n') != NULL)
+		removeNL(args[i - 1]);
+
+	pid_t pid = fork();
+
+	/*checks if fork failed*/
+	if (pid < 0) {
+		errPrint(1);
+		myExit(inpt);
+	} else if (pid == 0) { /*child process*/
+		execv(prgrm, args);
+		die(inpt);
+	} else { /*parent process*/
+		if (waitpid(pid, &st, 0) != pid) {
+			errPrint(1);
+			myExit(inpt);
+		}
+	}
+}
+
+/* removes next line from a string
  * Note that the only case this will happen is if one presses
- * enter at the very end due to how fgets works*/
-void removeNL(char *str){
-    str[strlen(str)-1] = 0;
-}
+ * enter at the very end due to how fgets works
+ */
+void removeNL(char *str) { str[strlen(str) - 1] = 0; }
 
-void errPrint(int realErr){
-    if(realErr)
-        fprintf(stderr, "error: %s\n", strerror(errno));
-    else
-        fprintf(stderr, "error: unknown command\n");
+void errPrint(int realErr)
+{
+	if (realErr)
+		fprintf(stderr, "error: %s\n", strerror(errno));
+	else
+		fprintf(stderr, "error: unknown command\n");
 
-    if(ferror(stderr)){
-        exit(EXIT_FAILURE);
-    }
+	if (ferror(stderr))
+		exit(EXIT_FAILURE);
 }
 
 /*Returns pointer to only the program name given a path*/
-char *progName(char *path){
-    char *delim = "/";
-    char *name= strtok(path, delim);
-    char *tmp; 
-    while((tmp=strtok(NULL, delim)) != NULL){
-        name = tmp;
-    }
+char *progName(char *path)
+{
+	char *delim = "/";
+	char *name = strtok(path, delim);
+	char *tmp;
 
-    if(strchr(name, '\n') != NULL)
-        removeNL(name);
-    return name;
+	while ((tmp = strtok(NULL, delim)) != NULL)
+		name = tmp;
+
+	if (strchr(name, '\n') != NULL)
+		removeNL(name);
+	return name;
 }
 
-int min(int a, int b){
-    if(a>b){
-        return b;
-    }
-    return a;
+int min(int a, int b)
+{
+	if (a > b)
+		return b;
+	return a;
 }
 
-
-void myExit(char *inpt){
-    free(inpt);
-    exit(EXIT_SUCCESS);
+void myExit(char *inpt)
+{
+	free(inpt);
+	exit(EXIT_SUCCESS);
 }
 
-void cd(char *inpt){
+void cd(char *inpt)
+{
 
-    if(inpt == NULL){
-        errPrint(0);
-        return ;
-    }
-    if(strchr(inpt, '\n') != NULL)
-        removeNL(inpt);
+	if (inpt == NULL) {
+		errPrint(0);
+		return;
+	}
+	if (strchr(inpt, '\n') != NULL)
+		removeNL(inpt);
 
-    if(chdir(inpt))
-        errPrint(1);
-
+	if (chdir(inpt))
+		errPrint(1);
 }
