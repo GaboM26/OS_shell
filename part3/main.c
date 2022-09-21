b973ea6bb493e7e7df67424165f854b47acd9abf
David Gabriel Millares Bellido <dgm2148@columbia.edu>
2022-09-21 14:42:43 -0400

    Part 3 Done, hello world centered + Makefile creates floppy.flp

--- part3/main.c
+++ part3/main.c
@@ -1,17 +1,17 @@
 void main(void){
     __asm__("mov $0xb800, %ax");
     __asm__("mov %ax, %ds");
-    int *x = (int *) 0;
+    int *x = (int *) 1992;
     *x=0xf065f068;
-    x=(int *)4;
+    x=(int *)1996;
     *x=0xf06cf06c;
-    x=(int *)8;
+    x=(int *)2000;
     *x=0xf02cf06f;
-    x=(int *)12;
+    x=(int *)2004;
     *x=0xf077f020;
-    x=(int *)16;
+    x=(int *)2008;
     *x=0xf072f06f;
-    x=(int *)20;
+    x=(int *)2012;
     *x=0xf064f06c;
     while(1){
 
