64e8efcf7dec66c25c5d184f679a7abe0a957c5f
David Gabriel Millares Bellido <dgm2148@columbia.edu>
2022-09-21 21:38:13 -0400

    Part 2 formatted, no checkpatch errors, documentation added

--- README.txt
+++ README.txt
@@ -133,3 +133,206 @@ rest of the documentation so you have access to tests I ran.
 -------------------------------------------------------------------------------
 Part 2|
 -------
+My w4118_sh2 works just as what is expected. I started with the sample of my
+part1 shell and removed all I/O functions one by one. The first I replaced
+was printf() with myPrint(). The next was fprintf() with errPrint(),
+a function I already had, but was now re-purposed to print out errors just
+like what the prompt required us to. errPrint() only prints out to stderr,
+while myPrint() only prints to stdout. The next function I implemented was
+myMalloc(), which calls mmap and is inspired by the bootlin code. I decided
+to not implement a function called myRealloc() as a replacement for the
+realloc() in getLine(), but instead just wrote the same functionality as
+realloc() within myGetLine(). myGetLine() calls myMalloc() and keeps
+allocating more space as needed. In order to test if this works, one can
+reduce the sizes of the buffer in the main function to 10 and the ones in
+the myGetLine() function to 3 each, and then run a 'long' command like
+$/bin/ls -lha /home/gabomba[or any directory in home]. Finally, instead of
+calling free, my file just directly runs munmap() whenever needed, including
+in myExit() function. 
+
+ASSUMPTION: I copied the shell from part 1 because the prompt in part 2 said
+"modify your simple shell". I understood that the implication is that we
+could copy the code I wrote in part 1 and modify it to call system calls
+directly. 
+
+SMALL ERROR: I did not find out how to deal with input in which the user
+presses Ctrl+D (EOF). The problem is that when this happens, I discovered
+that my "realloc()" part in myGetLine() would allocate 1000 more bytes for
+the buffer in the heap. This goes unnoticed for the first few times this is
+done, but could lead to problems as [Command] Ctrl+D presses go to infinity. 
+Apart from this very specific case, memory allocation doesn't grow 
+indefinitely at all. Furthermore, my testing showed me that all the
+allocated memory ended up being deallocated properly, so this was not an
+issue at all.
+
+NO ERRORS OR WARNINGS IN CHECKPATCH/VALGRIND
+...............................................................................
+Test run|
+---------
+COMMANDS I RAN:
+cd ..
+/bin/pwd
+/
+cd
+/bin
+/bin/cat references.txt
+cd /
+/bin/pwd
+/home/gabomba/a.out
+exit
+
+==5823== Memcheck, a memory error detector
+==5823== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
+==5823== Using Valgrind-3.16.1 and LibVEX; rerun with -h for copyright info
+==5823== Command: ./w4118_sh2
+==5823== 
+$$/home/gabomba/w4118/lab1/f22-hmwk1-GaboM26
+$error: Permission denied
+==5829== 
+==5829== HEAP SUMMARY:
+==5829==     in use at exit: 0 bytes in 0 blocks
+==5829==   total heap usage: 0 allocs, 0 frees, 0 bytes allocated
+==5829== 
+==5829== All heap blocks were freed -- no leaks are possible
+==5829== 
+==5829== For lists of detected and suppressed errors, rerun with: -s
+==5829== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+$error: unknown command
+$error: Permission denied
+==5831== 
+==5831== HEAP SUMMARY:
+==5831==     in use at exit: 0 bytes in 0 blocks
+==5831==   total heap usage: 0 allocs, 0 frees, 0 bytes allocated
+==5831== 
+==5831== All heap blocks were freed -- no leaks are possible
+==5831== 
+==5831== For lists of detected and suppressed errors, rerun with: -s
+==5831== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+$Reference List for W4118 Homework
+
+1.
+https://stackoverflow.com/questions/11824685/what-does-the-second-parameter-of-waitpid-mean,
+used in order to remember a bit how the waitpid function worked and what
+status was for
+
+2. https://cs3157.github.io/www/2022-1/resources/guides/git-tutorial.html
+Used in order for a refresher on how to use git
+
+3. https://linuxhint.com/check-malloc-error-c/
+I was unsure on how to handle a malloc error so I used this for reference
+
+4. http://www.cs.columbia.edu/~jae/3157/code/shell.c
+Im not sure if this must be cited because it was shown in class, but I put
+it here to be on the safe side. Looked at shell.c for the fork/exec lines of
+code
+
+5. https://elixir.bootlin.com/linux/latest/source/tools/include/nolibc/stdlib.h#L112
+Given with OS lab spec, used for help to implement malloc and realloc. Note
+that I don't directly implement a function named realloc, I just simply
+wrote code that does the same thing as realloc and placed it at the bottom of
+myGetLine() function
+
+6. https://commons.wikimedia.org/wiki/File:ASCII-Table-wide.svg
+Used to see the actual ASCII values of the corresponding chars to fill in the main.c file. 
+
+David Gabriel Millares Bellido
+DGM2148
+
+$$/
+$Hello World 
+$==5823== 
+==5823== HEAP SUMMARY:
+==5823==     in use at exit: 0 bytes in 0 blocks
+==5823==   total heap usage: 0 allocs, 0 frees, 0 bytes allocated
+==5823== 
+==5823== All heap blocks were freed -- no leaks are possible
+==5823== 
+==5823== For lists of detected and suppressed errors, rerun with: -s
+==5823== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+...............................................................................
+strace run|
+-----------
+I added an strace run with the same commands as before:
+
+execve("./w4118_sh2", ["./w4118_sh2"], 0x7ffe3fe57050 /* 30 vars */) = 0
+brk(NULL)                               = 0x55c349ddd000
+access("/etc/ld.so.preload", R_OK)      = -1 ENOENT (No such file or directory)
+openat(AT_FDCWD, "/etc/ld.so.cache", O_RDONLY|O_CLOEXEC) = 3
+fstat(3, {st_mode=S_IFREG|0644, st_size=73680, ...}) = 0
+mmap(NULL, 73680, PROT_READ, MAP_PRIVATE, 3, 0) = 0x7fd604683000
+close(3)                                = 0
+openat(AT_FDCWD, "/lib/x86_64-linux-gnu/libc.so.6", O_RDONLY|O_CLOEXEC) = 3
+read(3, "\177ELF\2\1\1\3\0\0\0\0\0\0\0\0\3\0>\0\1\0\0\0@n\2\0\0\0\0\0"..., 832) = 832
+fstat(3, {st_mode=S_IFREG|0755, st_size=1839792, ...}) = 0
+mmap(NULL, 8192, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) = 0x7fd604681000
+mmap(NULL, 1852680, PROT_READ, MAP_PRIVATE|MAP_DENYWRITE, 3, 0) = 0x7fd6044bc000
+mprotect(0x7fd6044e1000, 1662976, PROT_NONE) = 0
+mmap(0x7fd6044e1000, 1355776, PROT_READ|PROT_EXEC, MAP_PRIVATE|MAP_FIXED|MAP_DENYWRITE, 3, 0x25000) = 0x7fd6044e1000
+mmap(0x7fd60462c000, 303104, PROT_READ, MAP_PRIVATE|MAP_FIXED|MAP_DENYWRITE, 3, 0x170000) = 0x7fd60462c000
+mmap(0x7fd604677000, 24576, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_FIXED|MAP_DENYWRITE, 3, 0x1ba000) = 0x7fd604677000
+mmap(0x7fd60467d000, 13576, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS, -1, 0) = 0x7fd60467d000
+close(3)                                = 0
+arch_prctl(ARCH_SET_FS, 0x7fd604682540) = 0
+mprotect(0x7fd604677000, 12288, PROT_READ) = 0
+mprotect(0x55c3497f3000, 4096, PROT_READ) = 0
+mprotect(0x7fd6046bf000, 4096, PROT_READ) = 0
+munmap(0x7fd604683000, 73680)           = 0
+mmap(NULL, 100, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) = 0x7fd6046be000
+write(1, "$", 1)                        = 1
+read(0, "cd ..\n", 99)                  = 6
+chdir("..")                             = 0
+write(1, "$", 1)                        = 1
+read(0, "/bin/pwd\n", 99)               = 9
+clone(child_stack=NULL, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0x7fd604682810) = 5854
+wait4(5854, [{WIFEXITED(s) && WEXITSTATUS(s) == 0}], 0, NULL) = 5854
+--- SIGCHLD {si_signo=SIGCHLD, si_code=CLD_EXITED, si_pid=5854, si_uid=1000, si_status=0, si_utime=0, si_stime=0} ---
+write(1, "$", 1)                        = 1
+read(0, "/\n", 99)                      = 2
+clone(child_stack=NULL, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0x7fd604682810) = 5855
+wait4(5855, [{WIFEXITED(s) && WEXITSTATUS(s) == 0}], 0, NULL) = 5855
+--- SIGCHLD {si_signo=SIGCHLD, si_code=CLD_EXITED, si_pid=5855, si_uid=1000, si_status=0, si_utime=0, si_stime=0} ---
+write(1, "$", 1)                        = 1
+read(0, "cd\n", 99)                     = 3
+write(2, "error: unknown command\n", 23) = 23
+write(1, "$", 1)                        = 1
+read(0, "/bin\n", 99)                   = 5
+clone(child_stack=NULL, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0x7fd604682810) = 5856
+wait4(5856, [{WIFEXITED(s) && WEXITSTATUS(s) == 0}], 0, NULL) = 5856
+--- SIGCHLD {si_signo=SIGCHLD, si_code=CLD_EXITED, si_pid=5856, si_uid=1000, si_status=0, si_utime=0, si_stime=0} ---
+write(1, "$", 1)                        = 1
+read(0, "/bin/cat references.txt\n", 99) = 24
+clone(child_stack=NULL, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0x7fd604682810) = 5857
+wait4(5857, [{WIFEXITED(s) && WEXITSTATUS(s) == 0}], 0, NULL) = 5857
+--- SIGCHLD {si_signo=SIGCHLD, si_code=CLD_EXITED, si_pid=5857, si_uid=1000, si_status=0, si_utime=0, si_stime=0} ---
+write(1, "$", 1)                        = 1
+read(0, "cd /\n", 99)                   = 5
+chdir("/")                              = 0
+write(1, "$", 1)                        = 1
+read(0, "/bin/pwd\n", 99)               = 9
+clone(child_stack=NULL, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0x7fd604682810) = 5858
+wait4(5858, [{WIFEXITED(s) && WEXITSTATUS(s) == 0}], 0, NULL) = 5858
+--- SIGCHLD {si_signo=SIGCHLD, si_code=CLD_EXITED, si_pid=5858, si_uid=1000, si_status=0, si_utime=0, si_stime=0} ---
+write(1, "$", 1)                        = 1
+read(0, "/home/gabomba/a.out\n", 99)    = 20
+clone(child_stack=NULL, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0x7fd604682810) = 5859
+wait4(5859, [{WIFEXITED(s) && WEXITSTATUS(s) == 0}], 0, NULL) = 5859
+--- SIGCHLD {si_signo=SIGCHLD, si_code=CLD_EXITED, si_pid=5859, si_uid=1000, si_status=0, si_utime=0, si_stime=0} ---
+write(1, "$", 1)                        = 1
+read(0, "exit\n", 99)                   = 5
+munmap(0x7fd6046be000, 100)             = 0
+exit_group(0)                           = ?
++++ exited with 0 +++
+...............................................................................
+test run using executable|
+--------------------------
+2 of 2 tests passed
+5 of 5 tests passed
+9 of 9 tests passed
+
+Tests with this shell ran smoothly once mmap and munmap were figured out.
+Another mistake I faced early on was not null terminating input I received
+from read(). In the end, part 2 worked just as required.
+-------------------------------------------------------------------------------
+Part 3|
+-------
+
--- part2/shell2.c
+++ part2/shell2.c
@@ -1,265 +1,260 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<sys/types.h>
-#include<errno.h>
-#include<unistd.h>
-#include<sys/mman.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-void runProg(char *, size_t);
-void removeNL(char *);
-void errPrint(int );
-char *progName(char *);
-void validInput(char *, size_t);
-int min(int, int);
-void myExit(char *, size_t);
-void cd(char *);
-void myPrint(char *);
-void *myMalloc(size_t );
-int myGetLine(char **, size_t *);
-
-extern int errno;
-
-static void die(char *inpt, size_t sz){
-    errPrint(1);
-    myExit(inpt, sz);
-}
-
-int main(){
-    size_t size = 100;
-    int k;
-    char *inpt = (char *) myMalloc(size);
-
-    if(inpt == NULL){
-        errPrint(1);
-        exit(EXIT_FAILURE);
-    }
-
-    myPrint("$");
-    if(ferror(stdout))
-        errPrint(1);
-    while((k = myGetLine(&inpt, &size)) > 0){
-
-        /*in case getline used realloc*/
-
-        if(inpt == NULL){
-            errPrint(1);
-            exit(EXIT_FAILURE);
-        }
-
-        if(strcmp(inpt, "\n"))
-            validInput(inpt, size);
-        myPrint("$");
-        if(ferror(stdout))
-            errPrint(1);
-    }
-
-    if(k == -1 && errno != 0){
-        errPrint(1);
-        exit(EXIT_FAILURE);
-    }
-    /*if errno is 0 then no error occured*/
-    myExit(inpt, size);
-
-}
-
-/*something was entered as input (no blank line)
-  checks if it is valid and sends it to appropriate function */
-void validInput(char *inpt, size_t sz){
-
-    if(inpt[0]=='/'){
-        runProg(inpt, sz);
-        return ;
-    }
-    else if(inpt[0] == '.' && inpt[1] == '/'){
-        runProg(inpt, sz);
-        return ;
-    }
-    else if(inpt[0] == '.' && inpt[1] == '.' && inpt[2] == '/'){
-        runProg(inpt, sz);
-        return ;
-    }
-
-    /*Manipulate input now that we know its not trying to go to folder*/
-    char myInpt[strlen(inpt) + 1];
-    strcpy(myInpt, inpt);
-
-    char *spcmd = strtok(myInpt, " ");
-
-    if(strchr(spcmd, '\n') != NULL)
-        removeNL(spcmd);
-
-    if(!strcmp(spcmd, "exit"))
-        myExit(inpt, sz);
-    else if(!strcmp(spcmd, "cd"))
-        cd(strtok(NULL, " "));
-    else
-        errPrint(0);
-}
-void runProg(char *inpt, size_t size){
-    /*get command and save a copy to execute (manipulated pointers)*/
-    int st; 
-    char *tok = " ";
-    char *args[5000];
-    char *cmd = strtok(inpt, tok);    
-    char prgrm[strlen(cmd) + 1]; 
-    strcpy(prgrm, cmd);
-
-    /*parse and get all command line args*/
-    int i = 1;
-    while((args[i] = strtok(NULL, tok)) != NULL)
-        i++;
-    args[0] = progName(cmd); 
-
-    if(strchr(prgrm, '\n') != NULL)
-        removeNL(prgrm);
-
-    if(strchr(args[i-1], '\n') != NULL)
-        removeNL(args[i-1]);
-
-    pid_t pid = fork();
-
-    /*checks if fork failed*/
-    if(pid < 0){
-        errPrint(1);
-        myExit(inpt, size);
-    }
-    else if(pid == 0){ /*child process*/
-        execv(prgrm, args);
-        die(inpt, size);
-    }
-    else{ /*parent process*/
-        if(waitpid(pid, &st, 0) != pid){
-            errPrint(1);
-            myExit(inpt, size);
-        }
-    }
-
+int min(int a, int b)
+{
+	if (a > b)
+		return b;
+	return a;
 }
 
 /*removes next line from a string
  * Note that the only case this will happen is if one presses
- * enter at the very end due to how fgets works*/
-void removeNL(char *str){
-    str[strlen(str)-1] = 0;
+ * enter at the very end due to how getline works
+ */
+void removeNL(char *str) { str[strlen(str) - 1] = '\0'; }
+
+void myExit(char *inpt, size_t size)
+{
+	munmap(inpt, size);
+	exit(EXIT_SUCCESS);
 }
 
-void errPrint(int realErr){
-    char err[1000];
-    char start[] = "error: ";
-    strcpy(err, start);
-    if(!realErr){
-        strcat(err, "unknown command\n");
-    }
-    else{
-        strcat(err, strerror(errno));
-        strcat(err, "\n");
-    }
+void errPrint(int realErr)
+{
+	char err[1000];
+	char start[] = "error: ";
 
-    if(write(2, err, strlen(err)) != strlen(err)){
-        exit(EXIT_FAILURE);
-    }
+	strcpy(err, start);
+	if (!realErr) {
+		strcat(err, "unknown command\n");
+	} else {
+		strcat(err, strerror(errno));
+		strcat(err, "\n");
+	}
 
-    if(ferror(stderr)){
-        exit(EXIT_FAILURE);
-    }
+	if (write(2, err, strlen(err)) != strlen(err))
+		exit(EXIT_FAILURE);
+
+	if (ferror(stderr))
+		exit(EXIT_FAILURE);
 }
 
 /*Returns pointer to only the program name given a path*/
-char *progName(char *path){
-    char *delim = "/";
-    char *name= strtok(path, delim);
-    char *tmp; 
-    while((tmp=strtok(NULL, delim)) != NULL){
-        name = tmp;
-    }
+char *progName(char *path)
+{
+	char *delim = "/";
+	char *name = strtok(path, delim);
+	char *tmp;
 
-    if(strchr(name, '\n') != NULL)
-        removeNL(name);
-    return name;
+	while ((tmp = strtok(NULL, delim)) != NULL)
+		name = tmp;
+
+	if (strchr(name, '\n') != NULL)
+		removeNL(name);
+	return name;
 }
 
-int min(int a, int b){
-    if(a>b){
-        return b;
-    }
-    return a;
+static void die(char *inpt, size_t sz)
+{
+	errPrint(1);
+	myExit(inpt, sz);
 }
 
 
-void myExit(char *inpt,size_t size){
-    munmap(inpt, size);
-    exit(EXIT_SUCCESS);
+void cd(char *inpt)
+{
+
+	if (inpt == NULL) {
+		errPrint(0);
+		return;
+	}
+	if (strchr(inpt, '\n') != NULL)
+		removeNL(inpt);
+
+	if (chdir(inpt))
+		errPrint(1);
 }
 
-void cd(char *inpt){
+void *myMalloc(size_t size)
+{
+	void *hp;
 
-    if(inpt == NULL){
-        errPrint(0);
-        return ;
-    }
-    if(strchr(inpt, '\n') != NULL)
-        removeNL(inpt);
+	hp = mmap(NULL, size, PROT_READ | PROT_WRITE,
+			MAP_ANONYMOUS | MAP_PRIVATE, -1,
+			0);
+	if (__builtin_expect(hp == MAP_FAILED, 0))
+		return NULL;
 
-    if(chdir(inpt))
-        errPrint(1);
+	return hp;
 }
 
-void myPrint(char *msg){
-    if(write(1, msg, strlen(msg)) == -1){
-        exit(EXIT_FAILURE);
-    }
+void runProg(char *inpt, size_t size)
+{
+	/*get command and save a copy to execute (manipulated pointers)*/
+	int st;
+	char *tok = " ";
+	char *args[5000];
+	char *cmd = strtok(inpt, tok);
+	char prgrm[strlen(cmd) + 1];
+
+	strcpy(prgrm, cmd);
+
+	/*parse and get all command line args*/
+	int i = 1;
+
+	while ((args[i] = strtok(NULL, tok)) != NULL)
+		i++;
+	args[0] = progName(cmd);
+
+	if (strchr(prgrm, '\n') != NULL)
+		removeNL(prgrm);
+
+	if (strchr(args[i - 1], '\n') != NULL)
+		removeNL(args[i - 1]);
+
+	pid_t pid = fork();
+
+	/*checks if fork failed*/
+	if (pid < 0) {
+		errPrint(1);
+		myExit(inpt, size);
+	} else if (pid == 0) { /*child process*/
+		execv(prgrm, args);
+		die(inpt, size);
+	} else { /*parent process*/
+		if (waitpid(pid, &st, 0) != pid) {
+			errPrint(1);
+			myExit(inpt, size);
+		}
+	}
 }
 
-void *myMalloc(size_t size){
-    void *hp;
-    hp = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    if(__builtin_expect(hp == MAP_FAILED, 0))
-        return NULL;
+/*something was entered as input (no blank line)
+ *checks if it is valid and sends it to appropriate function
+ */
+void validInput(char *inpt, size_t sz)
+{
 
-    return hp;
+	if (inpt[0] == '/') {
+		runProg(inpt, sz);
+		return;
+	} else if (inpt[0] == '.' && inpt[1] == '/') {
+		runProg(inpt, sz);
+		return;
+	} else if (inpt[0] == '.' && inpt[1] == '.' && inpt[2] == '/') {
+		runProg(inpt, sz);
+		return;
+	}
+
+	/*Manipulate input now that we know its not trying to go to folder*/
+	char myInpt[strlen(inpt) + 1];
+
+	strcpy(myInpt, inpt);
+
+	char *spcmd = strtok(myInpt, " ");
+
+	if (strchr(spcmd, '\n') != NULL)
+		removeNL(spcmd);
+
+	if (!strcmp(spcmd, "exit"))
+		myExit(inpt, sz);
+	else if (!strcmp(spcmd, "cd"))
+		cd(strtok(NULL, " "));
+	else
+		errPrint(0);
 }
 
-int myGetLine(char **buff, size_t *size){
-    int bytrd;
-    char *wrkBuff = *buff;
-    /* EOF  */
-    if((bytrd = read(0, *buff, *size - 1)) == 0){
-        return 0;
-    }
-    else if(bytrd < 0){
-        /* read error */
-        return -1;
-    }
-    wrkBuff[bytrd] = '\0';
-    if(strchr(*buff, '\n'))
-        return bytrd;
+void myPrint(char *msg)
+{
+	if (write(1, msg, strlen(msg)) == -1)
+		exit(EXIT_FAILURE);
+}
 
-    /* buffer overflowed */
-    int newSize = *size + 1000;
-    char tempBuff[1000];
-    char *newBuff = myMalloc(newSize);
-    memcpy(newBuff, *buff, *size);
-    munmap(*buff, *size);
+int myGetLine(char **buff, size_t *size)
+{
+	int bytrd;
+	char *wrkBuff = *buff;
+	/* EOF  */
+	bytrd = read(0, *buff, *size - 1);
+	if (bytrd == 0) {
+		return 0;
+	} else if (bytrd < 0) {
+		/* read error */
+		return -1;
+	}
+	wrkBuff[bytrd] = '\0';
+	if (strchr(*buff, '\n'))
+		return bytrd;
 
-    while((bytrd = read(0, tempBuff, sizeof(tempBuff) - 1))  == sizeof(tempBuff) - 1){
-        if(strchr(tempBuff, '\n'))
-            break ;
-        tempBuff[bytrd] = '\0';
-        strcat(newBuff, tempBuff);
-        char *oldBuff = newBuff;
-        newSize = newSize + 1000;
-        newBuff = myMalloc(newSize); 
-        memcpy(newBuff, oldBuff, newSize -1000);
-        munmap(oldBuff, newSize-1000);
-        if(strchr(tempBuff, '\n'))
-            break ;
-    }
-    tempBuff[bytrd] = '\0';
-    strcat(newBuff, tempBuff);
-    *buff = newBuff;
-    *size = newSize;
-    return *size;
+	/* buffer overflowed */
+	int newSize = *size + 1000;
+	char tempBuff[1000];
+	char *newBuff = myMalloc(newSize);
+
+	memcpy(newBuff, *buff, *size);
+	munmap(*buff, *size);
+
+	while ((bytrd = read(0, tempBuff, sizeof(tempBuff) - 1)) ==
+			sizeof(tempBuff) - 1) {
+		if (strchr(tempBuff, '\n'))
+			break;
+		tempBuff[bytrd] = '\0';
+		strcat(newBuff, tempBuff);
+		char *oldBuff = newBuff;
+
+		newSize = newSize + 1000;
+		newBuff = myMalloc(newSize);
+		memcpy(newBuff, oldBuff, newSize - 1000);
+		munmap(oldBuff, newSize - 1000);
+		if (strchr(tempBuff, '\n'))
+			break;
+	}
+	tempBuff[bytrd] = '\0';
+	strcat(newBuff, tempBuff);
+	*buff = newBuff;
+	*size = newSize;
+	return *size;
+}
+
+int main(void)
+{
+	size_t size = 100;
+	int k;
+	char *inpt = (char *)myMalloc(size);
+
+	if (inpt == NULL) {
+		errPrint(1);
+		exit(EXIT_FAILURE);
+	}
+
+	myPrint("$");
+	if (ferror(stdout))
+		errPrint(1);
+	while ((k = myGetLine(&inpt, &size)) > 0) {
+
+		if (inpt == NULL) {
+			errPrint(1);
+			exit(EXIT_FAILURE);
+		}
+
+		if (strcmp(inpt, "\n"))
+			validInput(inpt, size);
+		myPrint("$");
+		if (ferror(stdout))
+			errPrint(1);
+	}
+
+	if (k == -1 && errno != 0) {
+		errPrint(1);
+		exit(EXIT_FAILURE);
+	}
+	/*if errno is 0 then no error occured*/
+	myExit(inpt, size);
 }
